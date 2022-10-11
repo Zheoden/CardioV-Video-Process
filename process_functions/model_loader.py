f590e6847610818f4772e192851307ca98d21a02
giuliposteraro <38227874+giuliposteraro@users.noreply.github.com>
2022-10-11 20:58:23 -0300

    subo modelo y script para cargar modelo

---
+++ process_functions/model_loader.py
@@ -0,0 +1,17 @@
+# ------------------------
+# Cargando modelo de disco
+# ------------------------
+import tensorflow as tf
+from keras.models import load_model
+
+def cargarModelo():
+
+    FILENAME_MODEL_TO_LOAD = "model_full.h5"
+    MODEL_PATH = "model/model_full.h5"
+
+    # Cargar la RNA desde disco
+    loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD)
+    print("Modelo cargado de disco << ", loaded_model)
+
+    graph = tf.get_default_graph()
+    return loaded_model, graph
