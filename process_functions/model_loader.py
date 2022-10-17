5596a3f2fa4169a1d76f2564d62ca82f9370a91f
giuliposteraro <38227874+giuliposteraro@users.noreply.github.com>
2022-10-17 19:03:35 -0300

    fix model_loader

--- process_functions/model_loader.py
+++ process_functions/model_loader.py
@@ -1,17 +1,18 @@
 # ------------------------
 # Cargando modelo de disco
 # ------------------------
+
 import tensorflow as tf
 from keras.models import load_model
+from model.utilities import focal_tversky, tversky
 
 def cargarModelo():
-
     FILENAME_MODEL_TO_LOAD = "model_full.h5"
-    MODEL_PATH = "model/model_full.h5"
+    MODEL_PATH = "../model"
 
     # Cargar la RNA desde disco
-    loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD)
+    loaded_model = load_model(MODEL_PATH + "/" + FILENAME_MODEL_TO_LOAD, custom_objects={"focal_tversky": focal_tversky,"tversky": tversky})
     print("Modelo cargado de disco << ", loaded_model)
 
-    graph = tf.get_default_graph()
+    graph = tf.compat.v1.get_default_graph()
     return loaded_model, graph
